d33609855d13655a9ac12534a1ddd7d762e97b10
Valeriia Seliverstova <valeriia.seliverstova.pz.2018@lpnu.ua>
2020-04-16 21:22:28 +0300

    Adding sessions

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'cloudinary'
 
 gem 'pusher'
+
+gem 'bcrypt-ruby'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root :to => 'home#index'
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'account/create'
+  get 'account/store'
   get 'post/create'
   get 'post/store'
   #get 'first/HelloWorld'
@@ -8,5 +16,16 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'post/create'
   post 'post/store'
+
+  get 'account/create'
+  post 'account/store'
+
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'account#create', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
