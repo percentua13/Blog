8447acd72d49a7597f17374772078b58f035aa7b
Valeriia Seliverstova <valeriia.seliverstova.pz.2018@lpnu.ua>
2020-04-17 19:48:57 +0300

    Adding "find by name/username"

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root :to => 'home#index'
 
 
+  get 'account/find', to: 'account#find'
+
   get 'account/update/:id', to: 'account#update'
   get 'account/info/:id', to: 'account#info'
   post 'account/updatesave', to: 'account#updatesave'
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
   get 'post/myposts'
   get 'post/create', to: 'post#create'
   get 'post/:id', to: 'post#currentpost'
+  get 'posts/:id', to: 'post#all_posts_of_selected_user'
+
   post '/comment/store/:id', to: 'comment#store'
 
   get 'sessions/new'
